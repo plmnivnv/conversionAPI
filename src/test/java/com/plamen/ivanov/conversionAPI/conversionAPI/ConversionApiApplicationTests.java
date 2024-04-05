b4f24125c5e52803a3ef8f5b3d60a05233d4d404
Plamen Ivanov <plamen.ivanov91@gmail.com>
2024-04-05 11:26:08 +0300

    Init commit

---
+++ src/test/java/com/plamen/ivanov/conversionAPI/conversionAPI/ConversionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plamen.ivanov.conversionAPI.conversionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
